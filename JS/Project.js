c29bc29568e4cd97776649962791a4ef98d7836f
Jared Clyde <greatwyrm@comcast.net>
2022-09-29 16:17:07 -0600

    Tried adding save function to JS

--- JS/Project.js
+++ JS/Project.js
@@ -29,4 +29,16 @@ cha.addEventListener('input', letter => {
 	chaOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
 
-//const btnInput = document.getElementById("btn");//THis is for saving to local storage
+//const btnInput = document.getElementById("btn");//This is for saving to local storage. Trying to save and return, but it doesn't work yet.
+
+const playInput = document.querySelector('input', 'textarea')
+const save = () => {
+    localStorage.setItem('charInput', playInput.textContent)
+}
+playInput.addEventListener('keydown', letter => {
+    playInput.textContent = letter.target.value
+}, save)
+const storedInput = localStorage.getItem('charInput')
+if (playInput) {
+    playInput.textContent = storedInput
+}
