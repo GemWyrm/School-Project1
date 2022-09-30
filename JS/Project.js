57bdd41eabce5b5ff8e9c28576b6de52839c566b
Jared Clyde <greatwyrm@comcast.net>
2022-09-30 11:22:36 -0600

    Linted JS file

--- JS/Project.js
+++ JS/Project.js
@@ -1,31 +1,31 @@
 //Math for each Ability Score
-const str = document.getElementById("str");
-const strOutput = document.getElementById("strOutput");
+const str = document.getElementById('str');
+const strOutput = document.getElementById('strOutput');
 str.addEventListener('input', letter => {
 	strOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
-const dex = document.getElementById("dex");
-const dexOutput = document.getElementById("dexOutput");
+const dex = document.getElementById('dex');
+const dexOutput = document.getElementById('dexOutput');
 dex.addEventListener('input', letter => {
 	dexOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
-const con = document.getElementById("con");
-const conOutput = document.getElementById("conOutput");
+const con = document.getElementById('con');
+const conOutput = document.getElementById('conOutput');
 con.addEventListener('input', letter => {
 	conOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
-const int = document.getElementById("int");
-const intOutput = document.getElementById("intOutput");
+const int = document.getElementById('int');
+const intOutput = document.getElementById('intOutput');
 int.addEventListener('input', letter => {
 	intOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
-const wis = document.getElementById("wis");
-const wisOutput = document.getElementById("wisOutput");
+const wis = document.getElementById('wis');
+const wisOutput = document.getElementById('wisOutput');
 wis.addEventListener('input', letter => {
 	wisOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
-const cha = document.getElementById("cha");
-const chaOutput = document.getElementById("chaOutput");
+const cha = document.getElementById('cha');
+const chaOutput = document.getElementById('chaOutput');
 cha.addEventListener('input', letter => {
 	chaOutput.textContent = Math.floor((letter.target.value - 10) / 2)
 });
